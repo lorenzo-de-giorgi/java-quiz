13eb7720e2afc8905b7de7b9dbc8e79ec4da9ff0
lorenzo-de-giorgi <lorenzodegiorgi2004@gmail.com>
2024-09-25 11:19:48 +0200

    created getters and setters for all private variables

--- Question.java
+++ Question.java
@@ -6,4 +6,49 @@ public class Question {
     private String opt3;
     private String opt4;
     private String answere;
+    
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getQuestion() {
+        return question;
+    }
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+    public String getOp1() {
+        return op1;
+    }
+    public void setOp1(String op1) {
+        this.op1 = op1;
+    }
+    public String getOpt2() {
+        return opt2;
+    }
+    public void setOpt2(String opt2) {
+        this.opt2 = opt2;
+    }
+    public String getOpt3() {
+        return opt3;
+    }
+    public void setOpt3(String opt3) {
+        this.opt3 = opt3;
+    }
+    public String getOpt4() {
+        return opt4;
+    }
+    public void setOpt4(String opt4) {
+        this.opt4 = opt4;
+    }
+    public String getAnswere() {
+        return answere;
+    }
+    public void setAnswere(String answere) {
+        this.answere = answere;
+    }
+
+    
 }
