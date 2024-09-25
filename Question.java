c4e5ecc829b07bbdedd0307ef6691256c5d4947d
lorenzo-de-giorgi <lorenzodegiorgi2004@gmail.com>
2024-09-25 11:18:33 +0200

    created scaffolding

---
+++ Question.java
@@ -0,0 +1,9 @@
+public class Question {
+    private int id;
+    private String question;
+    private String op1;
+    private String opt2;
+    private String opt3;
+    private String opt4;
+    private String answere;
+}
